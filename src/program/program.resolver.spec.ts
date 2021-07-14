51bc4ca7aee4c6f518719ed83b807378140656e7
Nicolas Coulange <coulange.n@gmail.com>
2021-07-14 21:22:20 +0200

    feat: resolver create program

--- src/program/program.resolver.spec.ts
+++ src/program/program.resolver.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { randomUUID } from 'crypto'
 import { ProgramResolver } from './program.resolver'
 import { ProgramService } from './program.service'
+import * as Faker from 'faker'
 
 describe('ProgramResolver', () => {
   let resolver: ProgramResolver
+  let programService: ProgramService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,16 +14,29 @@ describe('ProgramResolver', () => {
     }).compile()
 
     resolver = module.get<ProgramResolver>(ProgramResolver)
+    programService = module.get<ProgramService>(ProgramService)
   })
 
   it('should be defined', () => {
     expect(resolver).toBeDefined()
   })
 
-  it('should create a program', () => {
+  it('should create a program', async () => {
     const program = {
       title: 'Methode Lafay 3 semaines',
     }
-    expect(resolver.create(program))
+    const expectedProgram = {
+      id: expect.any(String),
+      title: program.title,
+    }
+
+    programService.create = jest.fn().mockResolvedValue({
+      id: Faker.datatype.uuid(),
+      title: program.title,
+    })
+
+    const createdProgram = await resolver.create(program)
+
+    expect(createdProgram).toStrictEqual(expectedProgram)
   })
 })
--- src/program/program.resolver.ts
+++ src/program/program.resolver.ts
@@ -3,7 +3,7 @@ import { Program } from './models/program.model'
 import { ProgramService } from './program.service'
 import { CreateProgramInput } from './types/create-program-input.type'
 
-@Resolver((of) => Program)
+@Resolver(() => Program)
 export class ProgramResolver {
   constructor(private readonly programService: ProgramService) {}
 
