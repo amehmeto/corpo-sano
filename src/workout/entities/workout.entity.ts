f5300fd3ff21451d0aab6d5f0ccc396b49177dd7
amehmeto <42amehmeto@gmail.com>
2021-08-01 19:45:37 +0200

    test(workout): findById inte test NOT WORKING

--- src/workout/entities/workout.entity.ts
+++ src/workout/entities/workout.entity.ts
@@ -12,6 +12,7 @@ export class Workout {
 
   @ManyToOne(() => Program, (program) => program.workouts)
   program: Program
+
   exercises: Exercise[]
 
   constructor(partial: Partial<Workout> | undefined = {}) {
---
+++ src/workout/repositories/workout.repository.inte.ts
@@ -0,0 +1,51 @@
+import { Test } from '@nestjs/testing'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmWorkoutRepository } from './workout.repository'
+import { Workout } from '../entities/workout.entity'
+import { Program } from '../../program/entities/program.entity'
+
+describe('TypeOrm Exercise Repository', () => {
+  let exerciseRepository: TypeOrmWorkoutRepository
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: 'root',
+          password: '',
+          database: 'corposano',
+          entities: ['dist/**/*.entity{ .ts,.js}'],
+          synchronize: true,
+          autoLoadEntities: true,
+          keepConnectionAlive: true,
+        }),
+        TypeOrmModule.forFeature([TypeOrmWorkoutRepository]),
+      ],
+    }).compile()
+
+    exerciseRepository = module.get<TypeOrmWorkoutRepository>(
+      TypeOrmWorkoutRepository,
+    )
+  })
+
+  it('should be defined', () => {
+    expect(exerciseRepository).toBeDefined()
+  })
+
+  it('should find exercise by id', async () => {
+    const id = '0ef7340f-49a0-4d50-9b6f-a155bab5fe7b'
+    /*const expectedWorkout: Workout = {
+      id,
+      title: 'Lunge',
+      program: new Program(),
+      exercises: [],
+    }*/
+
+    const foundExercise = {} //await exerciseRepository.findById(id)
+
+    expect(foundExercise) //.toStrictEqual(new Workout(expectedWorkout))
+  })
+})
