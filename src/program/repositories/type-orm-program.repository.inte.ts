119742b70f328870dd1d71959eb142f10b3e41a6
amehmeto <42amehmeto@gmail.com>
2021-09-26 18:01:35 +0300

    feat(program): get All Programs integrations tests

--- src/program/repositories/type-orm-program.repository.inte.ts
+++ src/program/repositories/type-orm-program.repository.inte.ts
@@ -6,6 +6,25 @@ import { Exercise } from '../../exercise/entities/exercise.entity'
 import { Program } from '../entities/program.entity'
 import { execSync } from 'child_process'
 import { Workout } from '../../workout/entities/workout.entity'
+import * as Faker from 'faker'
+
+const PROGRAM_FIXTURE_UUID = Faker.datatype.uuid()
+const PROGRAM_FIXTURE_UUID_2 = Faker.datatype.uuid()
+
+async function createProgramFixture(
+  programRepository: TypeOrmProgramRepository,
+) {
+  await programRepository.insert({
+    id: PROGRAM_FIXTURE_UUID,
+    title: 'Programme Hercule en 6 semaines',
+    workouts: [],
+  })
+  await programRepository.insert({
+    id: PROGRAM_FIXTURE_UUID_2,
+    title: 'Programme Aphrodite 2 semaines',
+    workouts: [],
+  })
+}
 
 describe('TypeOrm Program Repository', () => {
   let programRepository: TypeOrmProgramRepository
@@ -30,6 +49,7 @@ describe('TypeOrm Program Repository', () => {
 
   beforeAll(async () => {
     await execSync('yarn db:seed')
+    await createProgramFixture(programRepository)
   })
 
   afterAll(async () => {
@@ -38,5 +58,24 @@ describe('TypeOrm Program Repository', () => {
 
   it('should be defined', () => {
     expect(programRepository).toBeDefined()
+  })
+
+  it('should get all programs', async () => {
+    const expectedPrograms = [
+      new Program({
+        id: PROGRAM_FIXTURE_UUID_2,
+        title: 'Programme Aphrodite 2 semaines',
+        workouts: [],
+      }),
+      new Program({
+        id: PROGRAM_FIXTURE_UUID,
+        title: 'Programme Hercule en 6 semaines',
+        workouts: [],
+      }),
+    ]
+
+    const retrievedPrograms = await programRepository.getAllPrograms()
+
+    expect(retrievedPrograms).toStrictEqual(expectedPrograms)
   })
 })
