19223b09d9520928466a72eb3d866d73df6d8079
Erkam <erkam.bozan@gmail.com>
2022-03-16 15:17:37 +0300

    rebase feat/eslint

--- native-front/design-system/Avatar.tsx
+++ native-front/design-system/Avatar.tsx
@@ -1,17 +1,20 @@
 import { Image, StyleSheet } from 'react-native'
+import DefaultAvatar from '../assets/default-picture.png';
+import * as React from 'react'
 
 type AvatarProps = {
   source: string
 }
 
 export function Avatar({ source }: AvatarProps) {
+  if(source === 'default') source = DefaultAvatar
   return (
-    <Image
-      style={styles.avatar}
-      source={{
-        uri: source,
-      }}
-    />
+      <Image
+          style={styles.avatar}
+          source={{
+            uri: source,
+          }}
+      />
   )
 }
 
