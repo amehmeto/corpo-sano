0884f64a8d630f4e75a594e9e91ca9f37862c73c
amehmeto <42amehmeto@gmail.com>
2022-03-23 14:43:30 +0300

    review: athur's review changes

--- native-front/src/create-program/gateways/program.graphql.gateway.ts
+++ native-front/src/create-program/gateways/program.graphql.gateway.ts
@@ -13,14 +13,13 @@ export class GraphQLProgramGateway
 {
   async create(programInput: ProgramInput): Promise<Program> {
     try {
-      const CREATE_PROGRAM_MUTATION = `mutation CreateProgram($title: String!) {
+      const createProgramMutationPayload = {
+        query: `mutation CreateProgram($title: String!) {
           createProgram(title: $title) {
             id
             title
           }
-        }`
-      const createProgramMutationPayload = {
-        query: CREATE_PROGRAM_MUTATION,
+        }`,
         variables: {
           title: programInput.title,
         },
@@ -38,15 +37,13 @@ export class GraphQLProgramGateway
     workoutInput: WorkoutInput,
   ): Promise<Workout> {
     try {
-      const ADD_WORKOUT_MUTATION = `mutation CreateWorkout($title: String!, $programId: ID!) {
+      const addWorkoutMutationPayload = {
+        query: `mutation CreateWorkout($title: String!, $programId: ID!) {
           createWorkout(title: $title, programId: $programId) {
             id
             title
           }
-        }`
-
-      const addWorkoutMutationPayload = {
-        query: ADD_WORKOUT_MUTATION,
+        }`,
         variables: {
           title: workoutInput.title,
           programId,
@@ -67,15 +64,13 @@ export class GraphQLProgramGateway
   //TODO to be created in backend
   async find(): Promise<Program[]> {
     try {
-      const FIND_PROGRAMS_QUERY = `query GetAllPrograms {
+      const findProgramQueryPayload = {
+        query: `query GetAllPrograms {
           getAllPrograms {
             id
             title
           }
-        }`
-
-      const findProgramQueryPayload = {
-        query: FIND_PROGRAMS_QUERY,
+        }`,
         variables: {},
       }
 
@@ -85,8 +80,7 @@ export class GraphQLProgramGateway
     }
   }
 
-  //TODO should to create in backend
   findById(programId: string): Promise<Program | undefined> {
-    return Promise.resolve(undefined)
+    throw new Error('Method not implemented.')
   }
 }
--- native-front/src/home/mappers/athlete.mapper.ts
+++ native-front/src/home/mappers/athlete.mapper.ts
@@ -5,15 +5,13 @@ import { DailyTask } from '../entities/daily-task.entity'
 export class AthleteMapper {
   static mapToDomain(rawAthlete: any) {
     const rawBiometrics = rawAthlete.biometrics
-    const mappedDailyTasks = rawAthlete.dailyTasks.map(
-      (rawDailyTask: DailyTask) => {
-        return new DailyTask(
-          rawDailyTask.id,
-          rawDailyTask.description,
-          rawDailyTask.route,
-        )
-      },
-    )
+    const mappedDailyTasks = rawAthlete.dailyTasks.map((rawDailyTask: any) => {
+      return new DailyTask(
+        rawDailyTask.id,
+        rawDailyTask.description,
+        rawDailyTask.route,
+      )
+    })
 
     const mappedBiometrics = new Biometrics(
       rawBiometrics.bodyFat,
