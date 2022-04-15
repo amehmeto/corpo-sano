da047027165ae893a5a9fe0100f54f3e0397875b
Erkam <erkam.bozan@gmail.com>
2022-04-15 18:16:18 +0300

    feat(back-program): adding missing program repositories, type-orm, type-orm integreation test and in memory data

--- back/src/program/repositories/program-repository.interface.ts
+++ back/src/program/repositories/program-repository.interface.ts
@@ -1,9 +1,17 @@
 import { CreateProgramInput } from '../types/create-program-input.type'
 import { Program } from '../entities/program.entity'
+import { UpdateResult } from 'typeorm'
 
 export const PROGRAM_REPOSITORY = 'PROGRAM_REPOSITORY'
 
 export interface ProgramRepository {
   save(program: CreateProgramInput): Promise<Program>
+
+  getProgram(programId: string): Promise<Program>
+
   getAllPrograms(): Promise<Program[]>
+
+  find(): Promise<Program[]>
+
+  softDelete(programId: string): Promise<UpdateResult>
 }
--- back/src/program/repositories/type-orm-program.repository.inte.ts
+++ back/src/program/repositories/type-orm-program.repository.inte.ts
@@ -60,6 +60,15 @@ describe('TypeOrm Program Repository', () => {
     expect(programRepository).toBeDefined()
   })
 
+  it('should get a program', async () => {
+    const programId = programFixtures[0].id
+    const expectedProgram = new Program(programFixtures[0])
+
+    const retrievedProgram = await programRepository.getProgram(programId)
+
+    expect(retrievedProgram).toStrictEqual(expectedProgram)
+  })
+
   it('should get all programs', async () => {
     const expectedPrograms = [
       new Program(programFixtures[0]),
