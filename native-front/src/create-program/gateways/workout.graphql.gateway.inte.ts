0f62fe8678a0044bd2987140ae3056046cd58520
Erkam <erkam.bozan@gmail.com>
2022-04-16 23:05:56 +0300

    test(native-workout): adding missing workout integration tests and in-memory data

--- native-front/src/create-program/gateways/workout.graphql.gateway.inte.ts
+++ native-front/src/create-program/gateways/workout.graphql.gateway.inte.ts
@@ -5,6 +5,7 @@ import {
   deletePipe,
   initializeIntegrationTestEnvironment,
 } from '../../tests/initializeIntegrationTestEnvironment'
+import { HardCodedValuesEnum } from '../../tests/hard-coded-values.enum'
 
 describe('Workout Gateway', () => {
   let workoutGateway: GraphQLWorkoutGateway
@@ -23,7 +24,7 @@ describe('Workout Gateway', () => {
   })
 
   it('should get workout with id', async () => {
-    const workoutId = '06f7445d-ec29-4e81-bbdd-ce11897fb65d'
+    const workoutId = HardCodedValuesEnum.workoutId
     const expectedWorkout = expect.any(Workout)
 
     const retrievedWorkout = await workoutGateway.findById(workoutId)
@@ -32,12 +33,20 @@ describe('Workout Gateway', () => {
   })
 
   it('should update workout', async () => {
-    const workoutId = '06f7445d-ec29-4e81-bbdd-ce11897fb65d'
+    const workoutId = HardCodedValuesEnum.workoutId
     const workout = expect.any(Workout)
     const expectedWorkout = expect.any(Boolean)
 
     const retrievedWorkout = await workoutGateway.update(workoutId, workout)
 
     expect(retrievedWorkout).toStrictEqual(expectedWorkout)
+  })
+
+  it('should delete workout', async () => {
+    const workoutId = HardCodedValuesEnum.workoutId
+
+    const retrievedResult = await workoutGateway.delete(workoutId)
+
+    expect(retrievedResult).toBeTruthy()
   })
 })
