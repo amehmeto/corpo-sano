04fa3a680a628260d7db70fa8b0ec865814595a4
Erkam <erkam.bozan@gmail.com>
2022-05-03 07:28:55 +0300

    feat(create-program): adding new CreateExerciseScreen to create-program

---
+++ native-front/src/create-program/CreateExerciseScreen.tsx
@@ -0,0 +1,80 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { RestTimeSetter } from './components/RestTimeSetter'
+import { NumberSetter } from './components/NumberSetter'
+import { Button } from '../../design-system/Button'
+import { FontSize } from '../../design-system/enums/font-size.enum'
+import { RouteParams, Routes } from '../routers/HomeRouter'
+import { screenContainerStyle } from '../../design-system/screen-container.style'
+import { Exercise } from './entities/exercise.entity'
+import { GetExerciseUseCase } from './usecases/get-exercise.usecase'
+import { exerciseGateway } from '../_infrastructure/dependency-injection.container'
+import { ExerciseTemplate } from './entities/exercise-template.entity'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+const getExerciseUseCase = new GetExerciseUseCase(exerciseGateway)
+
+type CreateExerciseScreenProps = NativeStackScreenProps<
+  RouteParams,
+  Routes.CREATE_EXERCISE
+  >
+
+export default function CreateExerciseScreen({
+                                                 navigation,
+                                               }: CreateExerciseScreenProps) {
+
+  const [exercise, setExercise] = useState<Exercise>(
+    new Exercise(
+      '',
+      new ExerciseTemplate('', 'Abdominal'),
+      120,
+      0,
+      { minutes: 0, seconds: '04' },
+      { minutes: 0, seconds: '40' },
+    ),
+  )
+
+  useEffect(() => {
+  }, [])
+
+  function goToAddExercise() {
+    navigation.navigate(Routes.ADD_EXERCISE)
+  }
+
+  return (
+    <View style={screenContainerStyle.container}>
+      <Text style={styles.subTitle}>Exercise Title</Text>
+      <TextInput style={styles.textInput}/>
+
+      <Text style={styles.subTitle}>Number of sets</Text>
+      <NumberSetter _number={exercise.numberOfSets} />
+
+      <Text style={styles.subTitle}>Number of reps</Text>
+      <NumberSetter _number={exercise.numberOfReps} />
+
+      <Text style={styles.subTitle}>Inter sets rest time</Text>
+      <RestTimeSetter time={exercise.interSetsRestTime} />
+
+      <Text style={styles.subTitle}>Final rest time</Text>
+      <RestTimeSetter time={exercise.finalRestTime} />
+
+      <Button text={'Save Exercise'} onPress={goToAddExercise} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: FontSize.HEADING_2,
+  },
+  subTitle: {
+    fontWeight: 'bold',
+    fontSize: FontSize.HEADING_4,
+  },
+  textInput: {
+    display: 'flex',
+    backgroundColor: 'white',
+    height: 30,
+  }
+})
