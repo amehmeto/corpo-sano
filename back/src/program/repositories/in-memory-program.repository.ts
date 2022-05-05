810d784bc9d3a9d10d3e0959bf12eb8472177c67
Erkam <erkam.bozan@gmail.com>
2022-05-05 10:55:12 +0300

    refactor(back-program): refactoring programsdata, changing with databuilders

--- back/src/program/repositories/in-memory-program.repository.ts
+++ back/src/program/repositories/in-memory-program.repository.ts
@@ -2,12 +2,17 @@ import { ProgramRepository } from './program-repository.interface'
 import { CreateProgramInput } from '../types/create-program-input.type'
 import { Program } from '../entities/program.entity'
 import { v4 as uuid } from 'uuid'
-import { programFixtures } from '../data-builders/program.data-builder'
+import { programDataBuilder } from '../data-builders/program.data-builder'
 import { UpdateResult } from 'typeorm'
 import { Workout } from '../../workout/entities/workout.entity'
 
 export class InMemoryProgramRepository implements ProgramRepository {
-  private programsData = programFixtures
+  private programsData = [
+    programDataBuilder(),
+    programDataBuilder(),
+    programDataBuilder(),
+    programDataBuilder(),
+  ]
 
   private programs = this.programsData.map((data) => new Program(data))
 
@@ -15,7 +20,7 @@ export class InMemoryProgramRepository implements ProgramRepository {
     return Promise.resolve(new Program({ ...program, id: uuid() }))
   }
 
-  async saveWorkoutToProgram(
+  async updateProgram(
     programId: string,
     workout: Workout,
   ): Promise<Program> {
