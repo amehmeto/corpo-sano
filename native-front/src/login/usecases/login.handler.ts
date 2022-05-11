205167ca4c269c2ff10877608de3793bfc82863e
Erkam <erkam.bozan@gmail.com>
2022-05-11 14:04:55 +0300

    feat(native-login): adding login usecase

---
+++ native-front/src/login/usecases/login.handler.ts
@@ -0,0 +1,12 @@
+import { AuthCredentialsInput, LoginGateway } from '../gateways/login.gateway.interface'
+import AsyncStorage from '@react-native-community/async-storage'
+
+export class LoginUseCase {
+  constructor(private readonly loginGateway: LoginGateway) {
+  }
+
+  async execute(authCredentialsInput: AuthCredentialsInput) {
+    const token = await this.loginGateway.login(authCredentialsInput)
+    await AsyncStorage.setItem('token', token)
+  }
+}
