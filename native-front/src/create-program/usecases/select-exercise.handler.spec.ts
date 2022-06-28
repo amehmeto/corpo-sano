87fc09dcdab172dd46aa98b356db83fd51bd2eba
erkambozan <erkam.bozan@gmail.com>
2022-06-28 15:13:20 +0300

    Update select-exercise.handler.spec.ts

--- native-front/src/create-program/usecases/select-exercise.handler.spec.ts
+++ native-front/src/create-program/usecases/select-exercise.handler.spec.ts
@@ -8,11 +8,10 @@ describe('Select Exercise', () => {
       exerciseDataBuilder(),
     ]
 
-
-    const selectedExercises = selectWantedExercise(exercises, 0)
-
     const expectedExercises = exercises
     expectedExercises[0].isSelected = true
+
+    const selectedExercises = selectWantedExercise(exercises, 0)
 
     expect(expectedExercises).toStrictEqual(selectedExercises)
   })
