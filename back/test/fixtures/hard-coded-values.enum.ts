9a4ba9150bf0cc431ff3e3b4e79273e506ec6e42
amehmeto <42amehmeto@gmail.com>
2022-07-30 20:10:43 +0300

    chore: install eslint plugin react hook

--- back/test/fixtures/hard-coded-values.enum.ts
+++ back/test/fixtures/hard-coded-values.enum.ts
@@ -3,5 +3,5 @@ export enum HardCodedValuesEnum {
   programId = '23c8b6ce-9b10-465c-a581-44ca59d2c3ac',
   exerciseId = '131f2162-2d58-46f3-b941-6881f00d913a',
   athleteId = '93c87b16-9c92-4440-9ce3-658050ba8dd8',
-  athleteEmail = 'Shaniya46@hotmail.com',
+  athleteEmail = 'shaniya@gmail.com',
 }
